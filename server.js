d02160799715e1c5fb7810491c3422be4c577a27
tianchen zhang <tzhang98@myseneca.ca>
2022-10-05 17:34:12 -0400

    initial commit

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ var path = require("path")
 
 
 
-app.get("/",(req,res)=>{
-    res.send("Declaration(Instruction:text size in heading 2)");
-    res.send("Name: Tianchen Zhang(Tommy)");
-    res.send("Student ID: 101569218");
-}
-);
+
+app.get("/", (req, res)=>{
+    let resText =" <style>.bgr{background-color: yellow; }</style><h2>I declare that that this test is my own work in accordance with Seneca Academic Policy: No part of this teast has been copied manually or electronically from any other source.<h2/><br><h2>Name:Tianchen Zhang<br>Student Number:101569218</h2>"
+    resText += "<a href='/test1'> Go to Test1 </a>";
+    res.send(resText);
+});
 
 app.get("/test1",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/test1.html"));
+    res.sendFile(path.join(__dirname,"/view/test1.html"));
 }
 );
 
