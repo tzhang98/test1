d8cc9abfeb9e621ef59f69b9665784ea63e73a82
tianchen zhang <tzhang98@myseneca.ca>
2022-10-05 17:40:51 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+* BTI325 – test1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: ___TIANCHEN ZHANG________ Student ID: ___101569218_______ Date: __2022/10/5_______
+*
+* Online (Cyclic) URL:
+* __https://distinct-gold-bull.cyclic.app/
+*
+********************************************************************************/ 
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
@@ -9,7 +21,7 @@ var path = require("path")
 
 
 app.get("/", (req, res)=>{
-    let resText =" <style>.bgr{background-color: yellow; }</style><h2>I declare that that this test is my own work in accordance with Seneca Academic Policy: No part of this teast has been copied manually or electronically from any other source.<h2/><br><h2>Name:Tianchen Zhang<br>Student Number:101569218</h2>"
+    let resText =" <h2>I declare that that this test is my own work in accordance with Seneca Academic Policy: No part of this teast has been copied manually or electronically from any other source.<h2/><br><h2>Name:Tianchen Zhang<br>Student Number:101569218</h2>"
     resText += "<a href='/test1'> Go to Test1 </a>";
     res.send(resText);
 });
